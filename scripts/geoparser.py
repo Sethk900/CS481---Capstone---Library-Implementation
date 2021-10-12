f434722a9af4cb9c34a9b97b0a176aa3bc95ac01
root <root@journalmap480>
2021-10-12 00:21:01 +0000

    Minor improvements to documentation and removal of deprecated code/extra print statements

--- scripts/geoparser.py
+++ scripts/geoparser.py
@@ -12,46 +12,13 @@ geo = Geoparser()
 geoparse_single_file
 Accepts as input the path to a file of raw text representing the body of a given journal article. The function parses out any location names and writes them to an output file as a list of Python dictionaries.
 
-:param path: Path to a txt file containing raw text that represents the body of a given journal article.
+:param infilepath: Path to a txt file containing raw text that represents the body of a given journal article.
 :type path: str
+:param 
 '''
-def geoparse_single_file_deprecated(path) -> str:
-	inputfile = path
-	name, extension = os.path.splitext(inputfile)
-	outfilename = name + "_output.txt"
-	if xmlfile.match(inputfile): # Only process XML files
-		with open(inputfile, "r", encoding="utf-8") as infile:
-			print("Geoparsing data from " + inputfile + "...")
-			try:
-				data = infile.readlines().encode('utf-8')
-			except:
-				with open(outfilename, "w") as outfile:
-					outfile.write("Unicode Error")
-				outfile.close()
-				data = "none"
-		infile.close()
-		for line in data:
-			for word in line.split(): # For testing: eventually we'll want to parse a single word at a time to see what the geoparser is having a hard time with
-				output = geo.geoparse(str(word))
-				with open(outfilename, "w", errors='ignore') as outfile:
-					for line in output:
-						'''
-						IMPLEMENT FILTERS HERE
-						Filters, at least right now, are typically implemented using regular expressions. 
-						To implement one, you should build a regular expression that matches an attribute that you want to exclude from the geoparser output. 
-						Then, use the if statement below to filter out geonames that possess that attribute.
-						'''
-						if capital.match(line['word']): # Filter out place names that don't contain any capital later (Comment out to remove filter)
-							try:
-								outfile.write(str(line))
-								outfile.write("\n")
-							except:
-								print("Unicode error when trying to write word " + word['word'] + " to outfile.")
-
-				outfile.close()
-	return outfilename
-
-def geoparse_single_file(infilepath, outfilepath):
+def geoparse_single_file(infilepath: str, outfilepath: str = None) -> str:
+        if outfilepath is None:
+                outfilepath = infilepath.split('.')[0] + '_output.txt'
         outfile = open(outfilepath, 'w', encoding='utf-8')
         infile = open(infilepath, 'r', encoding = 'utf-8')
         content = infile.read()
