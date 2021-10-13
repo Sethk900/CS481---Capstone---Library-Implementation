51d3915dfc21b0c3b5d0821723143d8ae570b4e6
Seth King <king9778@vandals.uidaho.edu>
2021-10-13 16:41:44 -0700

    Small improvements to main analytics function

--- analytics.py
+++ analytics.py
@@ -1,3 +1,4 @@
+import os
 from scripts.geoparser import geoparse_single_file
 from scripts.xmlparser import xml_parse_single_file
 from scripts.analysis import analyze_single_file
@@ -9,12 +10,23 @@ Returns the predicted study location as a dictionary object.
 :param path: path to an XML file for which you want to predict the geographic study location
 :type path: str
 '''
-def generate_file_label(path) -> dict:
+def generate_file_label(path: str, keep_generated_files: bool = False) -> dict:
 	generated_files = []
 	raw_text_file = xml_parse_single_file(path)
 	generated_files.append(raw_text_file)
 	geoparser_results = geoparse_single_file(path, path.strip('\.txt') + '_output.txt')
 	predicted_study_location = analyze_single_file(geoparser_results)
 	print("Predicted Study Location: " + predicted_study_location[0]['word'])
-	return predicted_study_location
+	result = {}
+	filename = path.split('/')
+	filename = filename[len(filename)]
+	result[filename] = predicted_study_location
+
+	# If user doesn't want to keep the generated files, remove them
+	removal_string = "rm " + raw_text_file + " " + geoparser_results
+	os.system(removal_string)
+
+	# Return the article location label as a dict
+	return result
+
 
